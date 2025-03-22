39aa96e40e0d5b25979fe142b167a507e267e321
dmitray27 <dmitrayketz@gmail.com>
2025-03-22 16:35:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def get_led_status():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
+# , debug=True
